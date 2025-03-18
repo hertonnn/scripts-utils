114da46def84d00704cb28ee7ab023338f029416
hertonnn <hertonnn@github.com>
2025-03-18 17:29:42 -0300

    english all document

--- update_github.sh
+++ update_github.sh
@@ -8,7 +8,7 @@
 inf_repo(){
 	cd "$path"
 	cd ..
-	#verifica possíveis mudanças
+	#verify possible updates
 	if [ -z "$(git status --porcelain)" ] 
 	then
 		modified="false"
@@ -18,23 +18,23 @@ inf_repo(){
 	
 	repo_name=$(basename $(git rev-parse --show-toplevel))
 	
-	echo "$2 - Nome: $repo_name - Modificado: $modified"
+	echo "$2 - Name: $repo_name - Modified: $modified"
 	echo ""
 	
 }
 commit_repo(){
 	repo_path=$1
-	read -p "Digite a mensagem de commit para o repositório $repo_path: " commit_msg
+	read -p "Enter the mensage for this commit $repo_path: " commit_msg
 	cd "$repo_path" && cd .. && /usr/bin/git add -A && /usr/bin/git commit -m "$commit_msg" && /usr/bin/git push
 }
 update_all(){
 	for path in $@; do
 		commit_repo $path
 	done
-	echo "Repositório(s) atualizado(s) com sucesso!" 
+	echo "Reposity modified successfully!" 
 }
 # CODE-------------------------------------------------------------------- #
-function menuprincipal(){
+function menu(){
 clear
 echo " "
 repository_current="/home/herton/Documentos"
@@ -42,50 +42,50 @@ name_file=".git"
 
 
 
-# Use o comando find para procurar as pastas
+# Use the "find" command to search the folder 
 found_dir=$(find "$repository_current" -type d -name "$name_file" 2>/dev/null)
 
-# Verifica se a pasta foi encontrada
+# Check if the folder was found
 if [ -n "$found_dir" ]; then
-	echo ATUALIZAÇÃO DE REPOSITÓRIOS DO GITHUB
+	echo UPDATE GITHUB REPOSITORIES!
 	echo " "
-	echo Foram encontrados os seguintes repositórios git:
+	echo Found github repositories
 	num=1
 	for path in $found_dir; do
 		inf_repo $path $num
 		((num++))
 	done
-	echo "Press o número do repositório para atualizá-lo, zero(0) para atualizar todos os repositórios ou menos um (-1) para sair"
-	read opcao
+	echo "Enter the repository number to update, zero to update all repositories, or -1 to exit"
+	read option
 	
-	if [ $opcao -eq -1 ]; then
-		echo Saindo do sistema...
+	if [ $option -eq -1 ]; then
+		echo Exiting the system...
 		sleep 2
 		exit 0
 	fi
-	if [ $opcao -eq 0 ]; then
+	if [ $option -eq 0 ]; then
 		update_all $found_dir
 	else
 		var=1
 		for repo in $found_dir; do
-			if [ $var -eq $opcao ]; then 
+			if [ $var -eq $option ]; then 
 				commit_repo $repo
-				echo "Repositório(s) atualizado(s) com sucesso!" 
+				echo "Repositories updated successfully!" 
 				break
 			fi
 			((var++))
 		done 
 	fi
 	
-	menuprincipal
+	menu
 else
-  echo Pastas .git não encontradas
-  echo Saindo do sistema...
+  echo Do not found the git folders or No git folders found
+  echo Exiting the system...
   sleep 2
   exit 0
 fi
 
 }
-menuprincipal
+menu
 #END --------------------------------------------------------------------- #
 
